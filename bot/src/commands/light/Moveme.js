d63568c3d30306b9886300a929a2e6949079dd60
Alex6n <61611633+Alex6n@users.noreply.github.com>
2020-12-14 19:40:18 +0200

    Create Moveme.js

---
+++ bot/src/commands/light/Moveme.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "moveme",
+  description: "الانضمام الى روم اخر",
+  aliases: ["ودني", "اسحبني"],
+  async exec(message, args) {
+    const newchannel =  (args[1] ? message.guild.channels.cache.get(args[1]) || message.guild.channels.fetch(args[1]).catch(() => undefined) : undefined) || message.mentions.users.first().voice.channel
+    message.member.voice.channel.set(newchannel);
+  }
+};
