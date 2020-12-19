b929b40bb916d00b94926d7c63ac14aead46dc01
Mohammad Almajarish <rayzerpix@gmail.com>
2020-12-19 20:24:21 +0400

    Update Moveme.js

--- bot/src/commands/light/Moveme.js
+++ bot/src/commands/light/Moveme.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "الانضمام الى روم اخر",
   aliases: ["ودني", "اسحبني"],
   async exec(message, args) {
-    const newchannel =  (args[1] ? message.guild.channels.cache.get(args[1]) || message.guild.channels.fetch(args[1]).catch(() => undefined) : undefined) || message.mentions.users.first().voice.channel
+    const newchannel = (args[1] ? message.guild.channels.cache.get(args[1]) || message.guild.channels.fetch(args[1]).catch(() => undefined) : undefined) || message.mentions.users.first().voice.channel;
     message.member.voice.channel.set(newchannel);
   }
 };
