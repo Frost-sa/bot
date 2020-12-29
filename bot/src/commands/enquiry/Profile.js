414c47f170ef14c6056543197d63f9edf848ade2
Morningstar <61611633+Alex6n@users.noreply.github.com>
2020-12-29 11:53:46 +0000

    Update Profile.js

--- bot/src/commands/enquiry/Profile.js
+++ bot/src/commands/enquiry/Profile.js
@@ -18,7 +18,7 @@ module.exports = {
     const userAvatar = await resolveImage(user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 }));
     const { coins, rep, xp, bio } = await user.getProfile();
     const canvas = new Canvas(347, 218);
-    canvas.strokeStyle = 'rgba(0,0,0,0.5)';
+    canvas.strokeStyle = '#36393e';
     canvas.lineWidth = 50;
     canvas
       .printImage(backgroundImage, 0, 0, 347, 218)
@@ -44,10 +44,7 @@ module.exports = {
       .setTextAlign("center")
       .setTextSize(22)
       .printText(rep.likes > 0 ? "+" : rep.likes === 0 ? "" : "-", 174, 136)
-      .beginPath()
-      .moveTo(35, 161)
-      .lineTo(308, 161)
-      .stroke();
+      .strokeRect(35, 161, 308, 161);
     message.channel.send({ files: [{ attachment: canvas.toBuffer(), name: "profile.png" }] });
   }
 };
